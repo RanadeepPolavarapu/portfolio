02dff220949735ff0349235ed32c9b76e883c143
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 21:48:20 -0600

    Testing master banch checking

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Coming soon :)");
+  res.end("IF you can see this, my shit isn't together");
 });
 
 server.listen(port, hostname, () => {
