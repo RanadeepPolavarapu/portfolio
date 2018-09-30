91a5cddac50541ed7dc6a4be126a1db4b8601934
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 21:47:22 -0600

    Master branch check take 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Testing master brnach checking");
+  res.end("Testing master branch check on web server");
 });
 
 server.listen(port, hostname, () => {
