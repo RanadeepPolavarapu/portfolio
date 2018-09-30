621fcbaf1190ae58a5b32cff8be700034526eb57
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 22:25:52 -0600

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+export default () => (
+  <div>
+    <h1>About me</h1>
+    <ul>
+      <li>I'm 22 years old</li>
+      <li>I'm from Utah</li>
+      <li>I am a full-stack web developer</li>
+      <li>
+        I have 3 years of experience with working specifically with Ecommerce
+        sites (Shopify, WooCommerce, Magento, etc...)
+      </li>
+    </ul>
+    <h1>Who I've worked with</h1>
+    <p>
+      I've worked with many companies. Some I am legally not allowed to state,
+      but below are some that I have worked with:
+    </p>
+    <ul>
+      <li>Rumble Bros</li>
+      <li>Diesel Power Gear</li>
+      <li>Crossover Symmetry</li>
+    </ul>
+  </div>
+);
