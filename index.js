6618b3d7306b0004450ee010a1ce3212d600563d
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 20:53:59 -0600

    Coming soon

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Testing file watching");
+  res.end("Coming soon :)");
 });
 
 server.listen(port, hostname, () => {
