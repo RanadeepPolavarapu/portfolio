f0e06c0a0c587f7fa9c6df6f440ca74c7ae7b34b
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 21:50:00 -0600

    Reverted back to coming soon message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Testing master branch check on web server");
+  res.end("Coming soon :)");
 });
 
 server.listen(port, hostname, () => {
