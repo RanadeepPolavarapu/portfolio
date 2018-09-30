9d96f29d09c0a2ac888a327a7ecd47c2bcf6d72a
rumbletracy <tracy@rumblebrothers.com>
2018-09-29 22:25:59 -0600

    Added link element to link to other pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
     <p>This is where my portfolio will reside!</p>
   </div>
 );
